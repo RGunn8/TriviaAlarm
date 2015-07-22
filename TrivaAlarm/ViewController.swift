811fd82bcbb962c6a14567f7fcca984704560064
Ryan Gunn <ryanlgunn8@gmail.com>
2015-07-22 01:30:29 -0500

    Initial Commit

---
+++ TrivaAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrivaAlarm
+//
+//  Created by Ryan  Gunn on 7/22/15.
+//  Copyright (c) 2015 Ryan  Gunn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
